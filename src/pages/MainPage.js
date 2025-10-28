5315ee1470d5e789aa76a0a03cd177348969da98
김유라 <dbfk0214@gmail.com>
2025-10-28 12:13:31 +0900

    gitflow-feature-stash: mainone

---
+++ src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainComponents from '../components/MainComponents'
+
+const MainPage = () => {
+  return (
+    <div><MainComponents/></div>
+  )
+}
+
+export default MainPage
